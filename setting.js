a689045e8f88a9b77aac5b85daaa0d828f0149a3
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-06-08 12:41:47 +0700

    recode

--- setting.js
+++ setting.js
@@ -1,29 +1,27 @@
-const chalk = require('chalk')
-const fs = require('fs')
-
+const chalk = require("chalk");
+const fs = require("fs");
 
 // edit thumb ada di file string/ppbot.js
 
-global.owner = ['6285791762177'] // pakai nomor owner
-global.ownernomer = "6285791762177" // pakai nomor owner
-global.socialmedia = "IG: vlntncptr" // ini ig buat di ownerjy
-global.yutub = "YT: lenttobs"// sama
-global.lokasi = "Indonesia, Jawatimur, Lumajang"//
+global.owner = ["62895416187586"]; // pakai nomor owner
+global.ownernomer = "62895416187586"; // pakai nomor owner
+global.socialmedia = "IG: a.rakaabdirmp"; // ini ig buat di ownerjy
+global.yutub = "YT: Comprx"; // sama
+global.lokasi = "Indonesia, Jawabarat, Bandung"; //
 
-global.packgename = "by" 
-global.author = "lenttobs" 
+global.packgename = "by";
+global.author = "Rakarmp";
 
-global.title = 'SakuraMD'//Tittle reply 
-global.body = 'Subscribe lenttobs' //body reply
+global.title = "SakuraMD"; //Tittle reply
+global.body = "Arigato"; //body reply
 
-global.welcome = false 
-global.left = false
+global.welcome = false;
+global.left = false;
 
-
-let file = require.resolve(__filename)
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Telah di update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Telah di update'${__filename}'`));
+  delete require.cache[file];
+  require(file);
+});
