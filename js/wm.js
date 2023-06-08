c4b1b0a2f918de32542b99434c6d221c7410be99
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-06-08 13:02:36 +0700

    wm recode

--- js/wm.js
+++ js/wm.js
@@ -1,10 +1,10 @@
-const chalk = require('chalk')
-const fs = require('fs')
-global.wm = `Sakura Md By Lent :v` //lenttobs
-let file = require.resolve(__filename)
+const chalk = require("chalk");
+const fs = require("fs");
+global.wm = `Sakura-Md Recode Rakarmp Dari Kitagawa`; //lenttobs
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update'${__filename}'`));
+  delete require.cache[file];
+  require(file);
+});
